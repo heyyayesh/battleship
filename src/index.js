78bdd712af5288c4a21102d33de3a1a25be57734
heyyayesh <biswalayesh3@gmail.com>
2022-01-25 20:48:07 +0530

    organize code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import style from './styles/index.css';
-import gameLoop from './gameLoop';
+import gameLoop from './modules/gameLoop';
 
 const game = gameLoop();
 game.startGame();
--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-    entry: "./src/index.js",
+  entry: './src/index.js',
 
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: "images/[hash].[ext]",
-        clean: true,
-    },
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash].[ext]',
+    clean: true,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(css|scss)$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(jpg|jpeg|png|gif|svg)$/i,
-                type: 'asset/resource',
-            },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/i,
+        type: 'asset/resource',
+      },
 
-        ]
-    },
+    ],
+  },
 
-    devServer: {
-        static: path.resolve(__dirname, '../dist'),
-        hot: true,
-        watchFiles: path.resolve(__dirname, './src'),
-        open: true,
-    },
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+    watchFiles: path.resolve(__dirname, './src'),
+    open: true,
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/template.html'),
-            title: 'Battleship',
-        })
-    ]
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/template.html'),
+      title: 'Battleship',
+    }),
+  ],
+};
